01610e0e4fcbdb4353221b09fdc256d18229a778
brunasfenandes <bruna.fenandes@outlook.com>
2024-11-14 18:16:58 -0500

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Practice TypeScript 🤓",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Practice TailwindCSS 🤩",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Practice Practice Practice 👩🏻‍💻",
+    completed: false,
+  }
+]
